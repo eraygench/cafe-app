149b4679825624ad4af7e9790ac82bec242f383f
Eray Genç <eraygench@gmail.com>
2022-11-25 17:32:47 +0300

    navbar changes

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -2,14 +2,7 @@
     <header class="text-gray-600 body-font border-b-gray-600 border-b">
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
             <nav class="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto gap-x-5">
-                <Link :href="route('home')" :class="{ 'text-orange-600': route('home').endsWith($page.url) }">Home</Link>
-                <Link :href="route('sales')" v-if="!$page.props.auth.user.is_admin && !$page.props.auth.user.organization_id" :class="{ 'text-orange-600': route('sales').endsWith($page.url) }">Sales</Link>
-                <Link :href="route('users.index')" v-if="$page.props.auth.user.is_admin && !$page.props.auth.user.organization_id" :class="{ 'text-orange-600': route('users.index').endsWith($page.url) }">Users</Link>
-                <Link :href="route('organizations.index')" v-if="$page.props.auth.user.is_admin && !$page.props.auth.user.organization_id" :class="{ 'text-orange-600': route('organizations.index').endsWith($page.url) }">Organizations</Link>
-                <Link :href="route('products.index')" v-if="!$page.props.auth.user.is_admin && $page.props.auth.user.organization_id" :class="{ 'text-orange-600': route('products.index').endsWith($page.url) }">Products</Link>
-                <Link :href="route('categories.index')" v-if="!$page.props.auth.user.is_admin && $page.props.auth.user.organization_id" :class="{ 'text-orange-600': route('categories.index').endsWith($page.url) }">Categories</Link>
-                <Link :href="route('sections.index')" v-if="!$page.props.auth.user.is_admin && $page.props.auth.user.organization_id" :class="{ 'text-orange-600': route('sections.index').endsWith($page.url) }">Sections</Link>
-                <Link :href="route('plan')" v-if="!$page.props.auth.user.is_admin && $page.props.auth.user.organization_id" :class="{ 'text-orange-600': route('plan').endsWith($page.url) }">Desk Plan</Link>
+                <Link v-for="(route, key) in routes" v-if="route.visible" :key="key" :href="route.link" :class="{ 'text-orange-600': route.link.endsWith($page.url) }" v-text="route.text" />
             </nav>
             <a class="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
@@ -29,5 +22,47 @@
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            routes: [
+                {
+                    'link': !this.$page.props.auth.user.is_admin && this.$page.props.auth.user.organization_id ? route('plan') : route('home'),
+                    'text': 'Home',
+                    'visible': true
+                },
+                /*{
+                    'link': route('sales'),
+                    'text': 'Sales',
+                    'visible': !this.$page.props.auth.user.is_admin && !this.$page.props.auth.user.organization_id
+                },*/
+                {
+                    'link': route('users.index'),
+                    'text': 'Users',
+                    'visible': this.$page.props.auth.user.is_admin && !this.$page.props.auth.user.organization_id
+                },
+                {
+                    'link': route('organizations.index'),
+                    'text': 'Organizations',
+                    'visible': this.$page.props.auth.user.is_admin && !this.$page.props.auth.user.organization_id
+                },
+                {
+                    'link': route('products.index'),
+                    'text': 'Products',
+                    'visible': !this.$page.props.auth.user.is_admin && this.$page.props.auth.user.organization_id
+                },
+                {
+                    'link': route('categories.index'),
+                    'text': 'Categories',
+                    'visible': !this.$page.props.auth.user.is_admin && this.$page.props.auth.user.organization_id
+                },
+                {
+                    'link': route('sections.index'),
+                    'text': 'Sections',
+                    'visible': !this.$page.props.auth.user.is_admin && this.$page.props.auth.user.organization_id
+                }
+            ]
+        }
+    }
+}
 </script>
